e816b9d77e0840a9c6588829484adfa2f2651baa
Piggelinus <lericsson22@gmail.com>
2016-02-18 12:02:15 +0000

    index.php output added

--- algorithm.php
+++ algorithm.php
@@ -1,8 +1,34 @@
 <?php
 
+  #######################################
+ ##                                     ##
+### implementation of swanson algorithm ###
+ ##                                     ##
+  #######################################
+
+
 // require database connection code
 require('dbConnect.php');
 
 
+// sample query
+$query = "SELECT * FROM Animal LIMIT(25);";
+
+// execute query
+$result = $mysqli->query($sql);
+
+// process result
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+
+// close connection
+$mysqli->close();
 
 ?>
