918ba2536a017a15de2d4f6e0d4e52b05d4fd919
HUDSON G.T <g.t.hudson@durham.ac.uk>
2016-02-24 10:26:46 +0000

    Created correcy dbconnect.php

---
+++ dbConnect.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$servername = "mysql.dur.ac.uk";
+$username = "nobody";
+$password = "";
+$db = "Cljdw32_team7";
+
+// Create connection
+$mysqli = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+} 
+
+echo "Connected successfully";
+
+?>
