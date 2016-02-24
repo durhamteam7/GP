5df2bc203f55fb9df961834f1be8db8d974460f1
HUDSON G.T <g.t.hudson@durham.ac.uk>
2016-02-24 09:29:56 +0000

    Testing phpunit

--- UnitTest.php
+++ UnitTest.php
@@ -3,15 +3,13 @@ class UnitTest extends PHPUnit_Framework_TestCase
 {
     public function testPushAndPop()
     {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
 
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
+        $this->assertEquals(1, 2);
+    }
+    public function testPushAndPop2()
+    {
 
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
+        $this->assertEquals(1, 1);
     }
 }
 ?>
