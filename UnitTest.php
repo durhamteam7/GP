4470f05655e8ef1e93469e1304bfd765445c688d
HUDSON G.T <g.t.hudson@durham.ac.uk>
2016-02-24 09:53:49 +0000

    Added vendor to git ingore

--- UnitTest.php
+++ UnitTest.php
@@ -1,15 +1,37 @@
 <?php
 class UnitTest extends PHPUnit_Framework_TestCase
 {
-    public function testPushAndPop()
+    public function testCompare_by_classifiaction()
     {
-
         $this->assertEquals(1, 2);
     }
-    public function testPushAndPop2()
+    public function testGet_species_counts()
     {
-
-        $this->assertEquals(1, 1);
+        $this->assertEquals(1, 2);
+    }
+    public function testTally_spp_votes()
+    {
+        $this->assertEquals(1, 2);
+    }
+    public function testChoose_winners()
+    {
+        $this->assertEquals(1, 2);
+    }
+    public function testCalculate_num_animals()
+    {
+        $this->assertEquals(1, 2);
+    }
+    public function testCalculate_TF_perc()
+    {
+        $this->assertEquals(1, 2);
+    }
+    public function testWinner_info()
+    {
+        $this->assertEquals(1, 2);
+    }
+    public function testProcess_subject()
+    {
+        $this->assertEquals(1, 2);
     }
 }
 ?>
