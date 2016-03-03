01a96894ce7f1f5e8abfa51d890bf575734438a3
Thomas Hudson <g.t.hudson@durham.ac.uk>
2016-03-03 11:45:33 +0000

    Added more controllers

--- adminDash/app.js
+++ adminDash/app.js
@@ -1,20 +1,30 @@
 var adminApp = angular.module('adminDash', []);
 
-
-
 adminApp.controller('MainController', ['$scope', function($scope) {
   $scope.results = [];
 }]);
 
-
-
 adminApp.controller('FilterController', ['$scope', function($scope) {
   $scope.query = "in filter";
+
+  $scope.funFunction = function(a){
+	alert(a);
+	}
 }]);
 
 
-
-
 adminApp.controller('SearchResultsController', ['$scope', function($scope) {
+  $scope.var1 = "in search results";
+}]);
+
+adminApp.controller('SummaryController', ['$scope', function($scope) {
+  $scope.var1 = "in search results";
+}]);
+
+adminApp.controller('GraphsController', ['$scope', function($scope) {
+  $scope.var1 = "in search results";
+}]);
+
+adminApp.controller('CSVController', ['$scope', function($scope) {
   $scope.var1 = "in search results";
 }]);
