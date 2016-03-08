792037425f84a2da69c36b36bc8f337484819025
Piggelinus <lericsson22@gmail.com>
2016-03-08 16:17:47 +0000

    get_species_count test

--- tests/algorithmTest.php
+++ tests/algorithmTest.php
@@ -32,9 +32,11 @@ class SwansonTest extends PHPUnit_Framework_TestCase
 
     public function testGet_species_counts()
     {
-        $scals = array(array(array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
+        $array = array(array(array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "" => "animal")));
+        $array2 = array(array(array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "" => "")));
 
-        $this->assertEquals(10, $this->s->get_species_counts($scals));
+        $this->assertEquals(array(1), $this->s->get_species_counts($array));
+        $this->assertEquals(array(1), $this->s->get_species_counts($array2));
     }
     public function testTally_spp_votes()
     {
