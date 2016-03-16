1d4890f333620944969dbfc9c0f5819983276b57
Thomas Hudson <g.t.hudson@durham.ac.uk>
2016-03-16 12:58:57 +0000

    angularized species filter

--- adminDash/app.js
+++ adminDash/app.js
@@ -2,6 +2,7 @@ var adminApp = angular.module('adminDash', []);
 
 adminApp.controller('MainController', ['$scope', function($scope) {
 	$scope.results = [	];
+	$scope.species = [{"option_id":10,"option_name":"Badger"},{"option_id":11,"option_name":"Blackbird"},{"option_id":12,"option_name":"Domestic Cat"}]
 	$scope.getResults = function(query){
 		for (var i = 0; i < 10; i++) {
 			$scope.results.push({"photo_id": "1337", "upload_filename":"IMG_0003.JPG", "site":{"site_id":"1234"}, "upload":{"upload_id":"420", "person_id":"example"}, "taken":"01/02/16", "filename":"9ae715f0d99b9682a7978494ff43def6.jpg","dirname":"person_182/site_2"});
