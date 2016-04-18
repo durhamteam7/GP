d7d66a6910ac09f87aa3c9f662f693338fcb7a47
Thomas Hudson <g.t.hudson@durham.ac.uk>
2016-04-18 12:30:58 +0100

    Begun the map

--- userDash/app.js
+++ userDash/app.js
@@ -1,4 +1,6 @@
-var userApp = angular.module('userDash', ['rzModule', 'ui.bootstrap','googlechart',"checklist-model",'datetimepicker','ui.dashboard']);
+var userApp = angular.module('userDash', ['rzModule', 'ui.bootstrap',"checklist-model",'datetimepicker','leaflet-directive']);
+
+var urls = ["http://localhost:8080/","https://mammalweb.herokuapp.com/"];
 
 
 
@@ -22,7 +24,63 @@ userApp.factory('ajax', ['$http', function($http) {
 userApp.controller('MainController', ['$scope','ajax', function($scope,serverComm)
 {
 	//$("#loader").fadeTo("fast", 0.7);
-	$("#loader").fadeOut("slow");
+  // make map
 
-	$scope.id = "1";
+  var imgURL = "http://www.mammalweb.org/biodivimages/person_182/site_2/55fc9b6303e88ff8d0435eecc3e24683.jpg";
+  var html = '<img width=200 src="'+imgURL+'">';
+
+  var getHTML = function(imgURL){
+    var html = '<img width=200 src="http://www.mammalweb.org/biodivimages/'+imgURL+'">';
+    return html;
+  }
+
+  $scope.getResults = function(){
+    $("#loader").fadeTo("fast", 0.7);
+    serverComm.getPhotos({},1,100).success(function(data) {
+        console.log("Data:",data);
+        $scope.results = data.rows;
+        $scope.numResults = data.count;
+        for (var i = 0; i < $scope.results.length; i++) {
+          //Form correct URL
+          var result = $scope.results[i];
+          var parts = result.dirname.split("/");
+          $scope.results[i].URL = parts[parts.length - 2]+"/"+parts[parts.length - 1]+"/"+result.filename;
+
+          $scope.markers["m"+i] = {
+            lat: $scope.results[i].Site.lat,
+            lng: $scope.results[i].Site.lon,
+            message: getHTML($scope.results[i].URL)
+          }
+
+        }
+
+
+
+        $("#loader").fadeOut("slow");
+    });
+  };
+
+
+  angular.extend($scope, {
+        center: {
+            lat: 55,
+            lng: 0,
+            zoom: 7
+        },
+        markers:{},
+        tiles: {
+            url: "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        },
+        defaults: {
+            scrollWheelZoom: true
+        }
+    });
+
+  $scope.getResults();
+
+
+
+                
+
+
 }]);
