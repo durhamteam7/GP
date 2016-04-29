800d70adf7664c1829bbf6c626b2d94014e0d3fd
Piggelinus <lericsson22@gmail.com>
2016-04-29 10:38:39 +0100

    fixed testHighest_vote and testDecide_on

--- tests/algorithmTest.php
+++ tests/algorithmTest.php
@@ -23,6 +23,36 @@ class SwansonTest extends PHPUnit_Framework_TestCase
       $this->assertEquals(true, $this->s->setupDB());
     }
 
+    public function testTally_votes()
+    {
+        $empty_array = array();
+
+        $array = array(array("species" => ""));
+        $res = array("" => 1);
+
+        $array2 = array(array("species" => "animal"));
+        $res2 = array("animal" => 1);
+
+        $array3 = array(array("species" => "animal"),
+                        array("species" => "animal"));
+        $res3 = array("animal" => 2);
+
+        $array4 = array(array("species" => "animal"),
+                        array("species" => "species"));
+        $res4 = array("animal" => 1, "species" => 1);
+
+        $array5 = array(array("species" => "animal"),
+                        array("species" => "animal"),
+                        array("species" => "species"));
+        $res5 = array("animal" => 2, "species" => 1);
+
+        $this->assertEquals(array(), $this->s->tally_votes("species", $empty_array));
+        $this->assertEquals($res, $this->s->tally_votes("species", $array));
+        $this->assertEquals($res2, $this->s->tally_votes("species", $array2));
+        $this->assertEquals($res3, $this->s->tally_votes("species", $array3));
+        $this->assertEquals($res4, $this->s->tally_votes("species", $array4));
+        $this->assertEquals($res5, $this->s->tally_votes("species", $array5));
+    }
 
    	function testHighest_vote()
    	{
@@ -55,36 +85,36 @@ class SwansonTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(2, $this->s->highest_vote("species", $array5));
    	}
 
-    public function testTally_votes()
-    {
+    function testDecide_on()
+  	{
         $empty_array = array();
 
         $array = array(array("species" => ""));
         $res = array("" => 1);
 
-        $array2 = array(array("species" => "animal"));
+        $array2 = array(array("species" => "badger"));
         $res2 = array("animal" => 1);
 
-        $array3 = array(array("species" => "animal"),
-                        array("species" => "animal"));
+        $array3 = array(array("species" => "dog"),
+                        array("species" => "dog"));
         $res3 = array("animal" => 2);
 
-        $array4 = array(array("species" => "animal"),
-                        array("species" => "species"));
+        $array4 = array(array("species" => "deer"),
+                        array("species" => "cat"));
         $res4 = array("animal" => 1, "species" => 1);
 
-        $array5 = array(array("species" => "animal"),
-                        array("species" => "animal"),
-                        array("species" => "species"));
+        $array5 = array(array("species" => "deer"),
+                        array("species" => "deer"),
+                        array("species" => "badger"));
         $res5 = array("animal" => 2, "species" => 1);
 
-        $this->assertEquals(array(), $this->s->tally_votes("species", $empty_array));
-        $this->assertEquals($res, $this->s->tally_votes("species", $array));
-        $this->assertEquals($res2, $this->s->tally_votes("species", $array2));
-        $this->assertEquals($res3, $this->s->tally_votes("species", $array3));
-        $this->assertEquals($res4, $this->s->tally_votes("species", $array4));
-        $this->assertEquals($res5, $this->s->tally_votes("species", $array5));
-    }
+        $this->assertEquals("", $this->s->decide_on("species", $empty_array));
+        $this->assertEquals("", $this->s->decide_on("species", $array));
+        $this->assertEquals("badger", $this->s->decide_on("species", $array2));
+        $this->assertEquals("dog", $this->s->decide_on("species", $array3));
+        $this->assertEquals("cat", $this->s->decide_on("species", $array4));
+        $this->assertEquals("deer", $this->s->decide_on("species", $array5));
+  	}
 
     public function testCalculate_pielou()
     {
@@ -139,36 +169,5 @@ class SwansonTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(1/3, $this->s->fraction_support($array2));
         $this->assertEquals(0.5, $this->s->fraction_support($array3));
     }
-
-    function testDecide_on()
-  	{
-        $empty_array = array();
-
-        $array = array(array("species" => ""));
-        $res = array("" => 1);
-
-        $array2 = array(array("species" => "badger"));
-        $res2 = array("animal" => 1);
-
-        $array3 = array(array("species" => "dog"),
-                        array("species" => "dog"));
-        $res3 = array("animal" => 2);
-
-        $array4 = array(array("species" => "cat"),
-                        array("species" => "deer"));
-        $res4 = array("animal" => 1, "species" => 1);
-
-        $array5 = array(array("species" => "deer"),
-                        array("species" => "deer"),
-                        array("species" => "badger"));
-        $res5 = array("animal" => 2, "species" => 1);
-
-        $this->assertEquals("", $this->s->decide_on("species", $empty_array));
-        $this->assertEquals("", $this->s->decide_on("species", $array));
-        $this->assertEquals("badger", $this->s->decide_on("species", $array2));
-        $this->assertEquals("dog", $this->s->decide_on("species", $array3));
-        $this->assertEquals("cat", $this->s->decide_on("species", $array4));
-        $this->assertEquals("deer", $this->s->decide_on("species", $array5));
-  	}
 }
 ?>
