e110e9fa70a56e8841126d30bdf997e4aba77853
Thomas Hudson <g.t.hudson@durham.ac.uk>
2016-04-30 12:27:14 +0100

    Pushed missing common dependancies

---
+++ commonDependancies/serverComms.js
@@ -0,0 +1,37 @@
+var serverCommsModule = angular.module('serverComms',[]);
+
+//Allows for switching between dev local server and hosted server
+var urls = ["http://localhost:8080/", "https://mammalweb.herokuapp.com/"];
+var env = 1; // GLOBAL VARIABLE FOR ENVIRONMENT (0:Dev,1:Production)
+
+/**
+ * @memberof serverComms
+ * @ngdoc factory
+ * @name ajax
+ * @param {service} http Allows requests to api
+ * @return {object} functions
+ * @description
+ *   Makes http requests
+ */
+serverCommsModule.factory('ajax', ['$http', function($http) {
+    return {
+        getPhotos: function(query, pageNum, pageSize, isSequence) {
+            return $http.post(urls[env] + 'photo?pageNum=' + pageNum + '&pageSize=' + pageSize + '&sequence=' + isSequence, query).success(function() {});
+        },
+        getFullPhotos: function(query, isSequence) {
+            return $http.post(urls[env] + 'photo?sequence=' + isSequence, query).success(function() {});
+        },
+        getPhotosCSV: function(query, isSequence) {
+            return $http.post(urls[env] + 'photo?output=csv&sequence=' + isSequence, query).success(function() {});
+        },
+        getOptions: function() {
+            return $http.get(urls[env] + 'options').success(function() {});
+        },
+        getPersons: function() {
+            return $http.get(urls[env] + 'persons').success(function() {});
+        },
+        getFilters: function() {
+            return $http.get('../commonDependancies/filters.json').success(function() {});
+        }
+    };
+}]);
